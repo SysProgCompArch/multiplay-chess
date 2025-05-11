3a1a22f07723cf6cd3eaeb139c6d3056f6dffa9d
알파 <arpa1705@gmail.com>
2025-05-12 02:59:32 +0900

    feat: 빌드 스크립트 수정

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-cmake -S . -B build
-cmake --build build 
+
+if [ $# -eq 0 ]; then
+  cmake -S . -B build
+  cmake --build build --target client
+  cmake --build build --target server
+elif [ "$1" = "client" ] || [ "$1" = "server" ]; then
+  cmake -S . -B build
+  cmake --build build --target "$1"
+else
+  echo "Usage: $0 [client|server]"
+  exit 1
+fi 
