f456437e599bd6d44838911392ff6caf798e6c31
Buyeon Hwang <arpa1705@gmail.com>
2025-06-05 22:36:19 +0000

    fix(server): 채팅 메시지 핸들러에서 응답 전송 대상 수정 및 메모리 해제 추가

--- server/handlers/chat.c
+++ server/handlers/chat.c
@@ -7,42 +7,50 @@
 #include "match_manager.h"
 #include "network.h"
 
-// ECHO 메시지 처리 핸들러
 int handle_chat_message(int fd, ClientMessage *req) {
     if (req->msg_case != CLIENT_MESSAGE__MSG_CHAT) {
         LOG_ERROR("Invalid message type for chat handler: fd=%d, msg_case=%d", fd, req->msg_case);
         return -1;
     }
 
-    char *player_id = find_player_id_by_fd(fd);
-    if (!player_id) {
-        LOG_ERROR("Player ID not found for fd=%d", fd);
+    LOG_INFO("Chat received from fd=%d: %s", fd, req->chat->message ? req->chat->message : "(no message)");
+
+    ActiveGame *game = find_game_by_player_fd(fd);
+    if (!game) {
+        LOG_ERROR("Game not found for fd=%d", fd);
         return -1;
     }
 
-    LOG_INFO("Chat received from fd=%d: %s", fd, req->chat->message ? req->chat->message : "(no message)");
+    char *sender_id = game->white_player_fd == fd ? game->white_player_id : game->black_player_id;
 
     // Echo 응답 생성
     ServerMessage resp           = SERVER_MESSAGE__INIT;
     ChatBroadcast chat_broadcast = CHAT_BROADCAST__INIT;
 
+    resp.msg_case       = SERVER_MESSAGE__MSG_CHAT_BROADCAST;
+    resp.chat_broadcast = &chat_broadcast;
+
     // 메시지 복사
-    char *chat_message     = strdup(req->chat->message);
-    chat_broadcast.message = chat_message;
-    resp.msg_case          = SERVER_MESSAGE__MSG_CHAT_BROADCAST;
-    resp.chat_broadcast    = &chat_broadcast;
+    char *chat_message       = strdup(req->chat->message);
+    chat_broadcast.message   = chat_message;
+    chat_broadcast.player_id = sender_id;
 
     // 응답 전송
-    int result = send_server_message(fd, &resp);
+    int result = send_server_message(game->white_player_fd, &resp);
+    if (result < 0) {
+        LOG_ERROR("Failed to send chat response to fd=%d", game->white_player_fd);
+        return -1;
+    }
+
+    result = send_server_message(game->black_player_fd, &resp);
+    if (result < 0) {
+        LOG_ERROR("Failed to send chat response to fd=%d", game->black_player_fd);
+        return -1;
+    }
 
     // 메모리 해제
     free(chat_message);
 
-    if (result < 0) {
-        LOG_ERROR("Failed to send chat response to fd=%d", fd);
-        return -1;
-    }
-
-    LOG_DEBUG("Chat response sent to fd=%d", fd);
+    LOG_DEBUG("Chat response sent to fd=%d, %d", game->white_player_fd, game->black_player_fd);
     return 0;
 }
